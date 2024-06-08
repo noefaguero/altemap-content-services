c3fb5422c118bf99948552b92fc08cbc691c7442
noefaguero <noe.fernandez.aguero@gmail.com>
2024-06-08 11:03:40 +0200

    Multer configuration

--- src/config/multer.js
+++ src/config/multer.js
@@ -2,43 +2,39 @@ const multer = require('multer')
 
 
 const setFileName = (file, cb) => { // cb es la funcion de callback
-    const extension = file.originalname.split('.')[-1]
+    const extension = file.originalname.slice(file.originalname.lastIndexOf('.'))
     const filename = `${file.fieldname}_${Date.now()}.${extension}`
     cb(null, filename)
 }
 
+const setFileFilter = (req, file, cb, regex, str) => {
+    const extension = file.originalname.slice(file.originalname.lastIndexOf('.'))
+    const allowedTypes = regex
+
+    if (allowedTypes.test(extension) && allowedTypes.test(file.mimetype)) {
+        return cb(null, true)
+    } else {
+        cb(new Error(`Solo se permiten archivos ${str}`))
+    }
+}
+
+
+// configuracion de carga de imagenes
 exports.uploadImage = multer({
     storage: multer.diskStorage({
-        destination: (req, file, cb) => cb(null, 'uploads/delivery/'), // carpeta de entrega de contenidos
+        destination: (req, file, cb) => cb(null, `uploads/delivery/${req.params.id_project}`), // carpeta de entrega de contenidos del projecto
         filename: (req, file, cb) => setFileName(file, cb)
     }),
     limits: { fileSize: 1024 * 1024 * 1 }, // limite de 1MB
-    fileFilter: (req, file, cb) => {
-        const extension = file.originalname.split('.')[-1]
-        const allowedTypes = /jpeg|jpg|png|webp/i
-
-        if (allowedTypes.test(extension) && allowedTypes.test(file.mimetype)) {
-            return cb(null, true) // error nulo, se acepta
-        } else {
-            cb(new Error('Solo se permiten archivos jpeg, jpg y png'))
-        }
-    }
+    fileFilter: (req, file, cb) => setFileFilter(req, file, cb, '/jpeg|jpg|png|webp/i', 'jpeg, jpg y png')
 })
 
+// configuracion de carga de pdf
 exports.uploadPDF = multer({
     storage: multer.diskStorage({
         destination: (req, file, cb) => cb(null, 'uploads/delivery/'),
         filename: (req, file, cb) => setFileName(file, cb)
     }),
     limits: { fileSize: 1024 * 1024 * 5 }, // limite de 5MB
-    fileFilter: (req, file, cb) => {
-        const extension = file.originalname.split('.')[-1]
-        const allowedTypes = /pdf/i
-
-        if (allowedTypes.test(extension) && allowedTypes.test(file.mimetype)) {
-            return cb(null, true)
-        } else {
-            cb(new Error('Solo se permiten archivos pdf'))
-        }
-    }
+    fileFilter: (req, file, cb) => setFileFilter(req, file, cb, '/pdf/i', 'pdf')
 })
---
+++ src/constants.js
@@ -0,0 +1,6 @@
+exports.REGEXS = {
+    url: {
+        expression: 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#()?&//=]*)/',
+        message: 'URL no válida'
+    }
+}
