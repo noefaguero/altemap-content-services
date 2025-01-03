b3deb35a4c51dcb45e9e7bec5099e25d568653a5
noefaguero <noe.fernandez.aguero@gmail.com>
2025-01-03 18:42:27 +0100

    Functions to preserve consistency between elements and components

--- src/services/componentServices.js
+++ src/services/componentServices.js
@@ -1,18 +1,61 @@
 const Component = require('../database/models/componentModel')
+const { handleNotFound } = require('../utils/helpers')
 
-
-exports.getComponent = async (id, fields = null) => {
+exports.getComponentById = async (id) => {
     try {
-        return await Component.findById(id, fields).lean()
+		return handleNotFound(
+			await Component.findById(id).lean(),
+			"Componente no encontrado"
+		)
     } catch (error) {
         throw error
     }
 }
 
-exports.getComponentById = async (id) => {
+exports.getAllComponents = async () => {
     try {
-        return await Component.findById(id).lean()
+		return await Component.find().lean()
+
     } catch (error) {
         throw error
     }
+}
+
+// añadir referencia al insertar un nuevo elemento
+// se ejecuta en el middleware pre-save de elementModel
+exports.addElementReference = async (id, component_id) => {
+	try {
+		return handleNotFound(
+			await Component.findByIdAndUpdate(
+				component_id,
+				{
+					$push: { elements: id },
+					$inc: { elements_length: +1 }
+				}
+			),
+			"Componente no encontrado"
+		)
+	} catch (error) {
+		throw error
+	}
+}
+
+
+// eliminar clave ajena de un elemento si se elimina
+// se ejecuta en el middleware pre-remove de elementModel
+exports.removeElementReference = async (id, component_id) => {
+	try {
+		return handleNotFound(
+			await Component.findByIdAndUpdate(
+				component_id,
+				{
+					$pull: { elements: id },
+					$inc: { elements_length: -1 }
+				}
+			),
+			"Componente no encontrado"
+		)
+	} catch (error) {
+		throw error
+	}
 }
